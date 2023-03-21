673518b526e37b3472d8cb3464bdac1e30fe6071
Hanim <nazreen.hanim@celcom.com.my>
2023-03-21 16:36:48 +0800

    move homepage to index

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,7 +6,7 @@ export default function Navigation({ children }) {
     <main className="grid grid-cols-6 lg:grid-cols-12">
       <div className="lg:col-span-8 col-span-2"></div>
       <div className="flex justify-between col-span-4 rounded-bl-[30px] bg-white py-[20px] lg:px-[80px] px-[50px] text-black text-[12px] font-bold monospace uppercase">
-        <Link to="/home">Home</Link>
+        <Link to="/">Home</Link>
         <Link to="/home#portfolio">Portfolio</Link>
         <Link to="/about">About</Link>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,206 +1,85 @@
 import * as React from 'react'
+import Footer from '../components/footer'
+import Navigation from '../components/navigation'
+import { Link } from 'gatsby'
 
-const IndexPage = () => {
+const HomePage = () => {
   return (
-    <div className="bg-white text-dark raleway">
-      {/* Global Navs */}
-      <div className="grid grid-cols-2 lg:grid-cols-3 lg:pl-28 py-6 px-6 items-center">
-        <div className="text-left">Soundshape</div>
-        <div className="hidden lg:block">
-          <a href="" className="pr-20">
-            Home
-          </a>
-          <a href="" className="">
-            Support
-          </a>
-          <a href="" className="pl-20">
-            Service
-          </a>
-        </div>
-        {/* Product Button */}
-        <div className="text-right lg:pr-28">
-          <button href="" className="bg-gray-100 px-8 py-3">
-            <b>Our Products</b>
-          </button>
-        </div>
-      </div>
+    <main className="bg-black text-white">
+      {/* Navigation */}
+      <Navigation></Navigation>
 
       {/* Header */}
-      <div className="pt-20 lg:pt-28 grid lg:grid-cols-2 lg:gap-20 lg:pl-20 lg:pr-18">
-        {/* Left Side */}
-        <div className="px-6">
-          <h1 className="michroma text-7xl">
-            Collect
-            <br /> Your Rare
-            <br /> NFT Here! *
-          </h1>
-          <div className="grid grid-cols-2 pt-20 gap-20">
-            <div>
-              <b className="text-lg">Set Up Wallet</b>
-              <p className="mt-5">
-                Set up your own wallet through our protected system.
-              </p>
-            </div>
-            <div>
-              <b className="text-lg">Collection</b>
-              <p className="mt-5">
-                Add your favourite NFT to your collection from now!
-              </p>
-            </div>
-          </div>
-        </div>
+      <div
+        data-aos="flip-left"
+        className="pt-[100px] pb-[150px] flex justify-center text-[#CDDE91] text-[300px]"
+      >
+        ☺
+      </div>
 
-        {/* Right Side */}
-        <div>
-          <div className="grid grid-cols-2 pt-20 px-6">
-            <img src="" alt="icons" />
-            <p className="text-sm">
-              <b>800+</b> Users
-              <br /> Available Here
+      {/* Projects Involved List */}
+      <div
+        id="portfolio"
+        className="bg-white text-black rounded-t-[40px] py-[100px]"
+      >
+        <p className="monospace text-[20px] font-bold mb-[80px] lg:px-[100px] px-[40px]">
+          Selected Projects Involved <sup>05</sup>
+        </p>
+        {/* Portfolio One */}
+        <Link to="/portfolio/one">
+          <div className="grid lg:grid-cols-2 border-y border-[#bbb] lg:px-[100px] px-[40px] hover:bg-gradient-to-r from-white to-[#CDDE90]">
+            <p className="flex text-[30px] lg:text-[40px] uppercase py-[50px] lg:py-[70px]">
+              <b>Daily UI</b>
+              <i className="serif ml-[10px] mt-[-2.5px]">Challenges</i>
+            </p>
+            <p className="hidden lg:flex flex items-center justify-end monospace text-[12px] font-bold uppercase">
+              Read more ➚
             </p>
           </div>
-          <p className="pt-8 pb-20 lg:pb-10 px-6">
-            Get your own NFT here! We provide best
-            <br /> service marketplace 2022
-          </p>
-          <div className="grid lg:grid-cols-2">
-            <div className="bg-lime-300 px-6 lg:px-10 py-20">
-              <p className="michroma text-4xl">
-                See <br /> Bundle
-              </p>
-              <p className="uppercase text-sm pt-6">Aprodhium Bundle</p>
-            </div>
-            <div className="bg-black px-6 lg:px-10 py-28 hidden lg:block"></div>
+        </Link>
+        {/* Portfolio Two */}
+        <Link to="/portfolio/two">
+          <div className="grid lg:grid-cols-2 border-b border-[#bbb] lg:px-[100px] px-[40px] hover:bg-gradient-to-r from-white to-[#CDDE90]">
+            <p className="flex text-[30px] lg:text-[40px] uppercase py-[50px] lg:py-[70px]">
+              <b>Beige</b>
+            </p>
+            <p className="hidden lg:flex flex items-center justify-end monospace text-[12px] font-bold uppercase">
+              Read more ➚
+            </p>
           </div>
+        </Link>
+        {/* Portfolio Three */}
+        <Link to="/portfolio/three">
+          <div className="grid lg:grid-cols-2 border-b border-[#bbb] lg:px-[100px] px-[40px] hover:bg-gradient-to-r from-white to-[#CDDE90]">
+            <p className="flex text-[30px] lg:text-[40px] uppercase py-[50px] lg:py-[70px]">
+              <b>Boxy</b>
+              <i className="serif ml-[10px] mt-[-2.5px]">Shapes</i>
+            </p>
+            <p className="hidden lg:flex flex items-center justify-end monospace text-[12px] font-bold uppercase">
+              Read more ➚
+            </p>
+          </div>
+        </Link>
+      </div>
+
+      {/* Playground List */}
+      <div className="py-[100px] lg:px-[100px] px-[40px]">
+        <p className="monospace text-[20px] font-bold mb-[80px]">
+          Something From The Playground <sup>03</sup>
+        </p>
+        <div className="grid lg:grid-cols-3">
+          <div className="bg-white lg:mb-[0px] mb-[30px] lg:mr-[50px] rounded-t-[200px] rounded-b-[20px] h-[450px]"></div>
+          <div className="bg-white lg:mb-[0px] mb-[30px] lg:mr-[50px] rounded-b-[200px] rounded-t-[20px] h-[450px]"></div>
+          <div className="bg-white lg:mb-[0px] mb-[30px] lg:mr-[50px] rounded-t-[200px] rounded-b-[20px] h-[450px]"></div>
         </div>
       </div>
 
-      {/* Exclusive Bundle */}
-      <div className="bg-black grid lg:grid-cols-2 px-6 lg:px-20 py-28 lg:gap-20 text-white">
-        {/* Right side */}
-        <div className="">
-          <p className="michroma text-5xl">
-            Aprodhium
-            <br /> Exclusive Bundle
-          </p>
-          <p className="py-10">
-            See our new bunding now "Aprodhium". The new NFT collection that
-            will go to the moon for the next entire years. See complete package
-            containing lot of the type NFT inside.
-          </p>
-          <div className="">
-            <button className="bg-lime-300 text-black py-3 px-8">
-              <b>Discover Now</b>
-            </button>
-            <button className="pl-8 text-lime-300">
-              <b>See Other</b>
-            </button>
-          </div>
-        </div>
-
-        {/* Left Side */}
-        <div>img</div>
-      </div>
-
-      {/* Featured Works */}
-      <div className="bg-black grid lg:grid-cols-2 px-6 lg:px-20 py-28 lg:gap-20 text-white">
-        {/* Left Side */}
-        <div>img</div>
-
-        {/* Right Side */}
-        <div className="">
-          <p className="michroma text-5xl">
-            Featured
-            <br /> Works *
-          </p>
-          <p className="py-10">
-            Here is the new NFTs featured works from our top creator this month.
-            Check it and place your bid.
-          </p>
-
-          {/* Creator */}
-          <div className="grid grid-cols-2">
-            <div className="py-3">
-              <p className="text-xl">
-                <b>Pink Kamakuri</b>
-              </p>
-              <p className="text-sm">14 Edition Marked</p>
-            </div>
-            <div className="grid grid-cols-2">
-              <img src="" alt="icon" />
-              <div>
-                <p className="text-sm">Creator</p>
-                <p className="text-xl">
-                  <b>Parjow</b>
-                </p>
-              </div>
-            </div>
-          </div>
-
-          {/* Bid */}
-          <div className="grid grid-cols-2 items-center py-10">
-            <div>
-              <p className="text-sm">Starting Bid</p>
-              <p className="text-lime-300 text-3xl">
-                <b>1.5M</b>
-              </p>
-            </div>
-            <div>
-              <button className="bg-lime-300 text-black py-4 px-8">
-                <b>Place Bid</b>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Discover Now */}
-      <div className="bg-gray-100 pt-28 px-6 lg:px-28">
-        <div className="text-center">
-          <p className="michroma text-5xl">Discover Now!</p>
-          <p className="py-10">
-            Discover and find your NFTs collection and bid yours
-            <br /> from the list below
-          </p>
-        </div>
-
-        {/* Cards */}
-        <div className="grid lg:grid-cols-3 pt-10 pb-20 gap-16">
-          <div>
-            <div className="bg-lime-200">
-              <img src="" alt="bg-image" />
-            </div>
-            <div className="bg-white p-6">
-              <div className="grid grid-cols-2 items-center">
-                <div>
-                  <p className="text-lg">
-                    <b>Pink Kamakuri</b>
-                  </p>
-                  <p className="text-sm">14 Edition Marked</p>
-                </div>
-                <div className="text-right">
-                  <p>
-                    <b className="text-4xl pr-2">2.5</b>MYR
-                  </p>
-                </div>
-              </div>
-              <div className="grid grid-cols-2 items-center pt-6">
-                <div>
-                  <img src="" alt="icons" />
-                </div>
-                <div className="text-right text-sm">
-                  <p>14 Edition Marked</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div>2</div> <div>3</div>
-        </div>
-      </div>
-    </div>
+      {/* Footer */}
+      <Footer></Footer>
+    </main>
   )
 }
 
-export default IndexPage
+export default HomePage
 
-export const Head = () => <title>Test Page</title>
+export const Head = () => <title>Hanim</title>
