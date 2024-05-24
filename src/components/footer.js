f197ca57ddf0d61c85058d0fbe93c49f9c18b6f3
Hanim J. <work.nazreenhanim@gmail.com>
2024-05-24 13:14:14 +0000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ export default function Footer({ children }) {
       <div className="mb-[70px]">
         <div className="hidden lg:block bg-[#222] w-[200px] h-[200px] rounded-[200px]">
           <p className="text-[#CDDE91] -rotate-90 pl-[40px] pt-[5px] text-[100px]">
-            ➺
+            &#10170;
           </p>
         </div>
       </div>
@@ -15,8 +15,8 @@ export default function Footer({ children }) {
       {/* Contact */}
       <div>
         <p className="text-[28px] font-light">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis finibus
-          mauris elit, a accumsan dolor cursus.
+          Let's collaborate if you want to include sprinkles of magic and 
+          wizardry into your upcoming project.
         </p>
         {/* Email */}
         <p className="monospace text-[12px] uppercase mt-[70px]">Email Me</p>
