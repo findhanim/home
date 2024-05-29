9219d086644bfaecee06628f252fdd745ccf8831
Hanim J. <work.nazreenhanim@gmail.com>
2024-05-29 09:13:32 +0000

    Update file navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ export default function Navigation({ children }) {
   return (
     <main className="grid grid-cols-6 lg:grid-cols-12">
       <div className="text-[#CDDE91] text-[40px] pt-[28px] leading-[0px] flex justify-end col-span-1 mt-[20px] lg:pl-[80px] pl-[50px]">
-        <a href="/" data-replace="&#9786;">
+        <a href="/home" data-replace="&#9786;">
           <span>&#9786;</span>
         </a>
       </div>
@@ -13,10 +13,10 @@ export default function Navigation({ children }) {
         id="link"
         className="flex justify-between col-span-3 rounded-bl-[30px] bg-white py-[20px] lg:px-[80px] px-[50px] text-black text-[12px] font-bold uppercase"
       >
-        <a href="/#portfolio" data-replace="Portfolio">
+        <a href="/home/#portfolio" data-replace="Portfolio">
           <span>Portfolio</span>
         </a>
-        <a href="/about" data-replace="About">
+        <a href="/home/about" data-replace="About">
           <span>About</span>
         </a>
       </div>
